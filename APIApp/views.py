7c6dc078341174da2045295e40e5acd1d2d0d9cc
RahatSiddique2001 <rahatsiddique2001@gmail.com>
2023-07-18 23:32:25 +0600

    initial committtttttttt

---
+++ APIApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import generics
+
+# Create your views here.
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
